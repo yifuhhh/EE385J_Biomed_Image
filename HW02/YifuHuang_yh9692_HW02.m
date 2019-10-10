f2d4257ce81c8d433cb5ddc4f48d56e273df82f2
Yifu Huang <huangyifu@wireless-10-145-252-119.public.utexas.edu>
2019-10-10 12:25:36 -0500

    Trial 2 of HW02 Q01

--- HW02/YifuHuang_yh9692_HW02.m
+++ HW02/YifuHuang_yh9692_HW02.m
@@ -1,3 +1,4 @@
+%% Initialize
 clear;
 close all;
 clc;
@@ -20,40 +21,54 @@ time = [0, 1, 3, 4, 7]
 
 [sx, sy, sz, st] = size(control_SUV);
 
+%% ROI selection
+% place roi around  tumor, control
 figure
-imagesc(control_d7.roi_tumor(:,:,83)); axis image; axis off;
-
-% place roi around  tumor
+imagesc(control_d7.roi_tumor(:, :, 83)); axis image; axis off;
 title('Draw ROI around tumor','FontSize',20)
-[tumor_mask, tumor_x,tumor_y] = roipoly();
+[tumor_mask_c, tumor_x_c, tumor_y_c] = roipoly();
 close all
 
+% place roi around muscle, control
 figure
-imagesc(control_d7.SUV(:,:,83), [0, 3]); axis image; axis off;
-
-% place roi around muscle
+imagesc(control_d7.SUV(:, :, 83), [0, 3]); axis image; axis off;
 title('Draw ROI around muscle','FontSize',20)
-[muscle_mask, muscle_x, muscle_y] = roipoly(); 
-
+[muscle_mask_c, muscle_x_c, muscle_y_c] = roipoly(); 
 close all
 
-% calculate the mean value at each time point
+% place roi around  tumor, treat
+figure
+imagesc(treated_d0.roi_muscle(:, :, 40)); axis image; axis off;
+title('Draw ROI around tumor','FontSize',20)
+[tumor_mask_t, tumor_x_t, tumor_y_t] = roipoly();
+close all
+
+% place roi around muscle, control
+figure
+imagesc(treated_d7.SUV(:, :, 83), [0, 3]); axis image; axis off;
+title('Draw ROI around muscle','FontSize',20)
+[muscle_mask_t, muscle_x_t, muscle_y_t] = roipoly(); 
+close all
+
+%% calculate the mean value at each time point
 for t = 1:st % loop through each time point
     
-    d0 = control_SUV(:,:,83,1);
-    d = control_SUV(:,:,83,t);  % d is just a temporary variable;
+    d0 = control_SUV(:, :, 83, 1);
+    d = control_SUV(:, :, 83, t);  % d is just a temporary variable;
     
     % mean value of elements within the tumor mask;
-    tumor_tc(t,1) = mean(d(tumor_mask(:)))/mean(d0(tumor_mask(:)));
+    tumor_tc(t,1) = mean(d(tumor_mask_c(:)))/mean(d0(tumor_mask_c(:)));
     % 95% confidence interval +/- 1.96*standard_deviation/sqrt(n)
-    tumor_tc(t,2) = 1.96*std(d(tumor_mask(:)))/sqrt(sum(tumor_mask(:)));
+    tumor_tc(t,2) = 1.96*std(d(tumor_mask_c(:)))/sqrt(sum(tumor_mask_c(:)));
     
     
     % repeats this for muscle
-    muscle_tc(t,1) = mean(d(muscle_mask(:)))/mean(d0(muscle_mask(:)));
-    muscle_tc(t,2) = 1.96*std(d(muscle_mask(:)))/sqrt(sum(muscle_mask(:)));
+    muscle_tc(t,1) = mean(d(muscle_mask_c(:)))/mean(d0(muscle_mask_c(:)));
+    muscle_tc(t,2) = 1.96*std(d(muscle_mask_c(:)))/sqrt(sum(muscle_mask_c(:)));
 
 end
+
+%% Plot the mean values
 plot(time,tumor_tc(:,1),'-r',time,muscle_tc(:,1),'-b','LineWidth',2)
 hold on
 errorbar(time,tumor_tc(:,1),tumor_tc(:,2),'-r','LineWidth',2);
