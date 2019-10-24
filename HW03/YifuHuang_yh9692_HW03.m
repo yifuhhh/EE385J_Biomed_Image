47a05c9bc13b1c795b3a05f2f69db41e4771147f
Yifu Huang <huangyifu@wireless-10-146-0-61.public.utexas.edu>
2019-10-24 16:28:54 -0500

    Q 2d

--- HW03/YifuHuang_yh9692_HW03.m
+++ HW03/YifuHuang_yh9692_HW03.m
@@ -36,3 +36,62 @@ imagesc(base_s); axis image; title('Standard deviation','FontSize',20);
 
 % Q 03_b
 
+figure(2)
+imagesc(bm(:, :, 100));
+[tumor_mask, tumor_x, tumor_y] = roipoly();
+close all
+
+imagesc(bm(:,:,100)); axis image; axis off;
+axis image
+colormap parula
+title('Segment the tumor tissue','FontSize',20)
+xlabel('Position (mm)','FontSize',15)
+ylabel('Position (mm)','FontSize',15)
+set(gca,'FontSize',15)
+hold on
+plot(tumor_x, tumor_y, 'r', 'LineWidth', 2)
+hold off
+
+% Q 03_c
+
+max_tumor = zeros(75, 70);
+for y = 1 : sy
+    for x = 1 : sx
+        if tumor_mask(y, x) == 1
+            max_tumor(y, x) = (max(bm(y, x, :)))/(base_m(y, x));
+        else
+            max_tumor(y, x) = 100;
+        end
+    end
+end
+
+figure(3)
+imagesc(max_tumor, [1, 1.3]); axis image; axis off;
+axis image
+colormap hot
+colorbar
+
+% Q 03_d
+
+bd_tumor = zeros(75, 70, 4);
+for i = 1 : 4
+    for y = 1 : sy
+        for x = 1 : sx
+            if tumor_mask(y, x) == 1
+                bd_tumor(y, x, i) = base_m(y, x) + base_s(y, x) * 2^(i - 1);
+            else
+                bd_tumor(y, x) = 0;
+            end
+        end
+    end
+end
+
+figure(4)
+for i = 1 : 4
+    subplot(2, 2, i)
+    imagesc(bd_tumor(:, :, i)); axis image; axis off;
+    title(['Baseline + ' num2str(2^(i-1)) ' * SD'],'FontSize',15);
+end
+
+% Q 03_e
+
